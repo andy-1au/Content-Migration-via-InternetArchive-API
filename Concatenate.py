1b43229c557646a512a81e8f9a7a79e522d60dba
andy-1au <102329050+andy-1au@users.noreply.github.com>
2022-04-11 03:35:54 -0400

    Update Concatenate.py

--- Concatenate.py
+++ Concatenate.py
@@ -6,12 +6,16 @@ concatenatePath = 'S:\Digital Projects\Internet Archive Downloads\Alumni Bulleti
 
 all_filenames = [i for i in glob.glob('../Alumni Bulletin/CSV/*.csv')]
 df = pd.concat([pd.read_csv(f) for f in all_filenames])
-print(df)
+#---------TEMP SECTION TO CHANGE HEADERS----------
+print(df) #DEBUG
 df.set_axis(["Index", "Volume-Number", "Subject-Topic", "Description-Abstract", "Creator", "Date-Created", "Language-Code",
              "Rights", "Call-Number", "Identifier",	"PPI", "Capture-Device", "Date-Captured", "pid", "parent_object",
              "cmodel", "object_location", "label", "Title", "Type", "Year", "Language-Code", "Language-Text", "Format",
              "Format-URL", "File-Format", "Digital-Origin"	"Rights"], axis=1) #axis = 1 is for columns
-print(df)
+print(df) #DEBUG
+#---------TEMP SECTION TO CHANGE HEADERS---------- (can remove)
+#.1 occurs when we have the same headers
+# try using. df.rename(index: ' ', string: ' ') 
 df.to_csv(concatenatePath)
 
 
